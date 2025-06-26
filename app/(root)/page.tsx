dedc10b7613f46d7d5d3e46e24f73cf2b2586505
MIguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2025-06-26 16:38:29 -0600

    feat: Add global styles and components for layout structure
    - Implemented global CSS styles using Tailwind CSS for consistent theming and layout.
    - Created Bottombar, LeftSidebar, and RightSidebar components for the application layout.
    - Added middleware for authentication routes using Clerk.
    - Configured Tailwind CSS with custom theme settings and responsive design.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+export default function Home () {
+  return (
+    <>
+      <div>
+        Hello Threads
+      </div>
+    </>
+  )
+}
