7473719e74496bc25f059a2b39f61fb1e55db1a0
MIguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2025-06-27 06:43:50 -0600

    feat: Preview RightSidebar component implementation

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -1,6 +1,16 @@
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>
+          Suggested Communities
+        </h3>
+
+      <div className='mt-7 flex w-[350px] flex-col gap-9'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+        </div>
+      </div>
+    </section>
   )
 }
 
